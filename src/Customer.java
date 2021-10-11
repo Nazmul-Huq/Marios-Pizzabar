40753040b0965cb247131264dee2ea173a2b2fa2
SoesLy <slys87@gmail.com>
2021-10-11 13:27:56 +0200

    First draft of Customer & Menu

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,25 @@
 public class Customer {
+
+    private int id;
+    private String name;
+    private String adress;
+    private int phoneNumber;
+    private String email;
+
+    public void addCustomer(){
+
+
+    }
+
+    public void updateCustomer(){
+
+
+    }
+
+    public void deleteCustomer(){
+
+
+    }
+
+
 }
