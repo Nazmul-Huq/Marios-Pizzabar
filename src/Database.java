7aef9598fd60910402c61272d8094d74c69279a2
Nazmul-Huq <hmnazmulhuq@gmail.com>
2021-10-11 11:00:14 +0200

    database setup done

--- src/Database.java
+++ src/Database.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Database {
     static ArrayList<Customer> customerRecords = new ArrayList<Customer>();
-    static ArrayList<Customer> permanentCustomerRecord = new ArrayList<Customer>();
-    static ArrayList<Customer> orderRecord = new ArrayList<Customer>();
-    static ArrayList<Customer> menuDetail = new ArrayList<Customer>();
+    static ArrayList<Order> permanentCustomerRecord = new ArrayList<Order>();
+    static ArrayList<Order> orderRecord = new ArrayList<Order>();
+    static ArrayList<Menu> menuDetail = new ArrayList<Menu>();
 }
