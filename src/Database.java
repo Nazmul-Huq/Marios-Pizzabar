713b15491d5d9111495a6047d70fd126a288a7df
Nazmul-Huq <hmnazmulhuq@gmail.com>
2021-10-11 19:01:28 +0200

    permanentOrderRecord name correcte

--- src/Database.java
+++ src/Database.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Database {
     static ArrayList<Customer> customerRecords = new ArrayList<Customer>();
-    static ArrayList<Order> permanentCustomerRecord = new ArrayList<Order>();
+    static ArrayList<Order> permanentOrderRecord = new ArrayList<Order>();
     static ArrayList<Order> orderRecord = new ArrayList<Order>();
     static ArrayList<Menu> menuDetail = new ArrayList<Menu>();
 }
