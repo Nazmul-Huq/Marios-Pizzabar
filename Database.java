c8eb9df34d20065a6907640bacf6b5ef1ea372b8
Nazmul Huq <89709488+Nazmul-Huq@users.noreply.github.com>
2021-10-15 22:01:02 +0200

    database final

---
+++ Database.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Database {
+    /*
+    * this class written by Nazmul
+     */
+    static ArrayList<Customer> customerRecords = new ArrayList<Customer>();
+    static ArrayList<Order> permanentOrderRecord = new ArrayList<Order>();
+    static ArrayList<Order> orderRecord = new ArrayList<Order>();
+    static ArrayList<Pizza> pizzaDetail = new ArrayList<Pizza>();
+}
