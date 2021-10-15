fdd3ce1fe7561147ff3c6ff6d681f5fe31a67e5f
Nazmul Huq <89709488+Nazmul-Huq@users.noreply.github.com>
2021-10-15 22:00:42 +0200

    final

--- src/Database.java
+++ src/Database.java
@@ -7,6 +7,5 @@ public class Database {
     static ArrayList<Customer> customerRecords = new ArrayList<Customer>();
     static ArrayList<Order> permanentOrderRecord = new ArrayList<Order>();
     static ArrayList<Order> orderRecord = new ArrayList<Order>();
-    //static ArrayList<Menu> menuDetail = new ArrayList<Menu>(); // this data set name and type has changed as follows the below one
     static ArrayList<Pizza> pizzaDetail = new ArrayList<Pizza>();
 }
